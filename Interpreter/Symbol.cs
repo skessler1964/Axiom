2ef4890f226dfa387f9c03810d517fe34f0a0b01
skessler1964 <61694494+skessler1964@users.noreply.github.com>
2022-10-26 09:44:07 -0400

    Add files via upload

---
+++ Interpreter/Symbol.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// FileName : Symbol.cs
+// Author : Sean Kessler 
+
+namespace Axiom.Interpreter
+{
+  public class Symbol
+  {
+    public enum SymbolType { KeywordSymbol,FunctionSymbol, UserSymbol, UserDynamicSymbol, DirectiveSymbol};  
+    private bool isMutable=true;
+    private bool isModified=false;
+    public Symbol()
+    {
+      IsMutable = true;
+    }
+    public Symbol(String symbolName, Scanner.ScanSymbols identifier,SymbolType symbolType = SymbolType.UserSymbol)
+    {
+      this.SymbolName = symbolName;
+      this.Identifier = identifier;
+      this.TypeOfSymbol = symbolType;
+      if (symbolType.Equals(SymbolType.UserSymbol)) IsMutable = true;
+      else IsMutable = false;
+    }
+    public String SymbolName{get;set;}
+    public Scanner.ScanSymbols Identifier{get;set;}
+    public SymbolType TypeOfSymbol{get;set;}
+    public GenericData GenericData { get; set; }
+    public bool IsMutable
+    {
+      get { return isMutable; }
+      set {isMutable=value;}
+    }
+    public bool IsModified
+    {
+      get {return isModified;}
+      set {isModified=value;}
+    }
+  }
+}
