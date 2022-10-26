83cc525e64bb0a7c6f6c093f2a624d6642132b7b
skessler1964 <61694494+skessler1964@users.noreply.github.com>
2022-10-26 09:41:17 -0400

    Add files via upload

---
+++ Interpreter/AxiomResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+// FileName : AxiomResult.cs
+// Author : Sean Kessler
+
+namespace Axiom.Interpreter
+{
+  public class AxiomResult
+  {
+    public AxiomResult()
+    {
+      Success = false;
+    }
+    public AxiomResult(bool success,String lastMessage)
+    {
+      Success = success;
+      LastMessage = lastMessage;
+    }
+    public bool Success { get; set; }
+    public String LastMessage { get; set; }
+    public DataTable DataTable { get; set; }
+    public int ContextSpecificId{get;set;}
+    public Object ContextSpecificObj { get; set; }
+  }
+}
