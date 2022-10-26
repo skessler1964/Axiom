0cff7c5048b03dd46b76d07ad2d8735e108c3063
skessler1964 <61694494+skessler1964@users.noreply.github.com>
2022-10-26 09:43:46 -0400

    Add files via upload

---
+++ Interpreter/StackElement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+// FileName : StackElement.cs
+// Author : Sean Kessler
+
+namespace Axiom.Interpreter
+{
+  public class CodeStack
+  {
+    private Stack<StackElement> codeStack = new Stack<StackElement>();
+    public CodeStack()
+    {
+    }
+    public void Push(StackElement stackElement)
+    {
+      codeStack.Push(stackElement);
+    }
+    public StackElement Pop()
+    {
+      return codeStack.Pop();
+    }
+  }
+  public class StackElement
+  {
+    public StackElement()
+    {
+    }
+    public GenericData Value { get; set; }
+    public Symbol Symbol { get; set; }
+  }
+}
