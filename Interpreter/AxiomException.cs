57e92a4a4b20f79db42f25a9c8c7f7fd6780b0a4
skessler1964 <61694494+skessler1964@users.noreply.github.com>
2022-10-26 09:40:50 -0400

    Add files via upload

---
+++ Interpreter/AxiomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+// FileName : AxiomException.cs
+// Author : Sean Kessler
+
+namespace Axiom.Interpreter
+{
+  public class AxiomException : Exception
+  {
+    public AxiomResult AxiomResult{get;set;}
+    public AxiomException()
+    {
+    }
+    public AxiomException(AxiomResult axiomResult,Exception exception)
+    : base(exception.Message,exception)
+    {
+      this.AxiomResult=axiomResult;
+    }
+  }
+}
